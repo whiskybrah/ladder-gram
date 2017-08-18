b219a4b346734b71c3129c144cbd55d9346d483c
Nathan <hatchet190@hotmail.com>
2017-08-18 20:40:57 +1000

    file checking

--- word_ladder.py
+++ word_ladder.py
@@ -1,19 +1,20 @@
 from collections import defaultdict
 from itertools import product
 import unittest
+import os.path
 from collections import deque
 
 def startsearch(network, start): # uses breadth-first search
-    nodevisited = set()  # create a set that keeps track of which vertices have been visited already
+    nodevisited = set()  # create a set that keeps track of which arcs have been visited already
     q = deque([[start]])  # create a queue which contains all the paths from starting arc, created with a list
-                          # that contains the start vertex (named start)
+                          # that contains the start arc (named start)
     while q: # this begins the growing of paths, one at a time
         node = q.popleft()  # pop a path from the queue to continue exploring
                             # and retrieve the last arc visited from that path
         arc = node[-1]
         yield arc, node
         for nearby in network[arc] - nodevisited: # retrieve neighbours from network,
-                                                  # remove the vertices that have already been visited
+                                                  # remove the arcs that have already been visited
             nodevisited.add(nearby) # then for each of the remaining unvisted neighbours the arc is set to
                                     # visited and a path consisting of the path so far plus the arc is added
             q.append(node + [nearby])   # adding the new arc schedules it for further exploration,
@@ -30,7 +31,7 @@ def createGraph(wordList):
         for x in range(len(word)):
             container = '{}~{}'.format(word[:x], word[x + 1:]) # given a tilde wildcard, a single container
             # is defined by words that are similar within single characters
-            # e.g. lea~ container would have -> leak, lean, leap, lead and more vice versa
+            # e.g. lea~ container would have -> leak, lean, leap, lead and more
             containers[container].append(word)
             # add arcs and edges for words in the same container
     for container, nearby in containers.items():
@@ -41,8 +42,12 @@ def createGraph(wordList):
                 network[firstword].add(secondword)
     return network
 
-# dictfile = input("Enter dictionary:")
-dictionary = createGraph(wordretrieve('dictionary.txt'))
+dictfile = input("Enter dictionary:")
+if os.path.isfile(dictfile):
+    dictionary = createGraph(wordretrieve(dictfile))
+else:
+    print("Incorrect file name")
+    exit(0)
 
 if __name__ == '__main__':
     startword = input("Enter start word:")
@@ -51,10 +56,3 @@ if __name__ == '__main__':
         if arc == targetword:
             print( ' -> '.join(path))
 
-
-class myTest(unittest.TestCase):
-  def test_graph(self):
-      print("setup")
-
-
-unittest.main()
